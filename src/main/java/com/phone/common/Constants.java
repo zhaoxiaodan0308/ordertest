38e589f25b191ce8b07d819bba663d4044533f7d
zhaoxiaodan <apanman_0308@163.com>
2018-09-19 17:12:36 +0800

    zhaoxiaodan

---
+++ src/main/java/com/phone/common/Constants.java
@@ -0,0 +1,17 @@
+package com.phone.common;
+
+/**
+ * FileName: Constants
+ * Author: zhao
+ * Date: 2018/9/19 10:25
+ * Description:整个项目常量类
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+public class Constants {
+    private final static String LOG_IP="ip";
+    private final static String LOG_SERVER_TIME="s_";
+    private final static String LOG_EVENT_NAME="en";
+
+}
